365153310fb4ce77eca86791580c155bdf4a242f
Anirudh Jwala <anirudhsai2@gmail.com>
2024-02-25 01:55:55 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Anirudh Jwala on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
