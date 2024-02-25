1dfb305803756dc9943306ad9e7ab31c32a0dbf0
Anirudh Jwala <anirudhsai2@gmail.com>
2024-02-25 12:48:50 +0530

    Buttons and properties

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -1,26 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State // property wrapper
+    var showExchangeInfo = false // stored property
+    
     var body: some View {
         ZStack {
             // Background image
             Image(.background)
                 .resizable() // view modifiers
                 .ignoresSafeArea()
-
+            
             VStack {
                 // Prancing pony image
                 Image(.prancingpony)
                     .resizable()
                     .scaledToFit()
                     .frame(height: 200)
-
+                
                 // Currency exchange text
                 Text("Currency Exchange")
                     .font(.largeTitle)
                     .foregroundStyle(.white)
                     .fontWeight(.semibold)
-
+                
                 // Currency conversion section
                 HStack {
                     // Left conversion section
@@ -32,23 +35,23 @@ struct ContentView: View {
                                 .resizable()
                                 .scaledToFit()
                                 .frame(height: 33)
-
+                            
                             // Currency text
                             Text("Silver Piece")
                                 .font(.headline)
                                 .foregroundStyle(.white)
                         }
-
+                        
                         // Textfield
                         Text("Textfield")
                     }
-
+                    
                     // Equal sign
                     Image(systemName: "equal")
                         .font(.largeTitle)
                         .foregroundStyle(.white)
                         .symbolEffect(.pulse)
-
+                    
                     // Right conversion section
                     VStack {
                         // Currency
@@ -57,27 +60,36 @@ struct ContentView: View {
                             Text("Gold Piece")
                                 .font(.headline)
                                 .foregroundStyle(.white)
-
+                            
                             // Currency image
                             Image(.goldpiece)
                                 .resizable()
                                 .scaledToFit()
                                 .frame(height: 33)
                         }
-
+                        
                         // Textfield
                         Text("Textfield")
                     }
                 }
                 
                 Spacer()
-
-                // Info button
-                Image(systemName: "info.circle.fill")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
+                
+                HStack {
+                    Spacer()
+                    
+                    // Info button
+                    Button {
+                        showExchangeInfo.toggle()
+                        print("showExchangeInfo value: \(showExchangeInfo)")
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
             }
-            .border(.blue)
         }
     }
 }
