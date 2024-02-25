a4ed09a0248e10221ba10e58651b7403b47c4b04
Anirudh Jwala <anirudhsai2@gmail.com>
2024-02-25 10:54:31 +0530

    Layouting our main screen

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -2,17 +2,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                .padding()
-            Text("Hello, world!")
-            Button("Button") {
+        ZStack {
+            // Background image
+            
+            VStack {
+                // Prancing pony image
                 
+                // Currency exchange text
+                
+                // Currency conversion section
+                HStack {
+                    // Left conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency image
+                            
+                            // Currency text
+                        }
+                        
+                        // Textfield
+                    }
+                    
+                    // Equal sign
+                    
+                    // Right conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency text
+                            
+                            // Currency image
+                        }
+                        
+                        // Textfield
+                    }
+                }
+
+                // Info button
             }
+
         }
-        .padding()
     }
 }
 
